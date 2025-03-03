66aab3f137765e647f6cc802e0f6887dbe677f57
Mateus Santos <mateuspele2015@gmail.com>
2025-03-02 23:37:42 -0300

    features: added new routes

--- src/notifications/notifications.service.ts
+++ src/notifications/notifications.service.ts
@@ -1,33 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { UpdateNotificationDto } from './dto/update-notification.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class NotificationsService {
+  constructor(private readonly prisma: PrismaService) {}
 
-async create(createNotificationDto: { level: number }) {
-  const level = createNotificationDto.level;
-  const levelInMeters = level / 100;
-  const data = {
-    "to": "ExponentPushToken[ASg0RAGPuH8n6dtmlKXK6i]",
-    "sound": "default",
-    "title": "🌊 Alerta de Enchente",
-    "body": `O nível do rio está em ${levelInMeters.toFixed(2)} metros. Fique atento!`,
-    "data": {
-      "level": levelInMeters.toFixed(2),
-      "nav": "/teste"
-    }
-  };
-
-  const response = await fetch('https://exp.host/--/api/v2/push/send', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(res => res.json());
-
-  return response;
-}
+  async create(createNotificationDto: { level: number }) {
+    const level = createNotificationDto.level;
+    const levelInMeters = level / 100;
+  
+    // Obtem todos os usuários do banco
+    const users = await this.prisma.user.findMany();
+  
+    // Mapeia e envia notificações para cada usuário
+    const notifications = users.map(async (user) => {
+      const data = {
+        to: user.deviceToken, // Token do dispositivo do usuário
+        sound: "default",
+        title: "🌊 Alerta de Enchente",
+        body: `O nível do rio está em ${levelInMeters.toFixed(2)} metros. Fique atento!`,
+        data: {
+          level: levelInMeters.toFixed(2),
+          nav: "/teste",
+        },
+      };
+  
+      // Envia a notificação
+      const response = await fetch("https://exp.host/--/api/v2/push/send", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+  
+      return response; // Retorna a resposta para cada usuário
+    });
+  
+    // Aguarda todas as notificações serem enviadas
+    const results = await Promise.all(notifications);
+  
+    return results; // Retorna os resultados de todas as notificações
+  }
+  
 
   async findAll() {
    return `This action returns all notifications`;  
